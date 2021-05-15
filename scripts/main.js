3a7c2b51ea3cb700d147784c96b8632352308e65
adamschwarcz <34557306+adamschwarcz@users.noreply.github.com>
2021-05-16 01:08:25 +0200

    Assignment result

---
+++ scripts/main.js
@@ -0,0 +1,38 @@
+var rotationSpeed = 0.001;
+var myOtherBox = document.getElementById("myOtherBox");
+
+function spin() {
+  myOtherBox.object3D.rotation.x += rotationSpeed;
+  myOtherBox.object3D.rotation.y += rotationSpeed;
+  myOtherBox.object3D.rotation.z += rotationSpeed / 2;
+  //console.log(myOtherBox.object3D.rotation);
+}
+setInterval(spin, 10);
+
+myOtherBox.addEventListener("mouseenter", function () {
+  rotationSpeed = 0.01;
+  console.log("entered");
+});
+
+myOtherBox.addEventListener("mouseleave", function () {
+  rotationSpeed = 0.001;
+  console.log("left");
+});
+
+/*
+ * click-events can use a fuse (default on mobile). So the event only fires, if the cursor is on the object for a specified time.
+ */
+
+var growspeed = 10;
+
+function grow() {
+  myOtherBox.object3D.rotation.x -= growspeed;
+  myOtherBox.object3D.rotation.y += growspeed;
+  console.log(myOtherBox.object3D.scale);
+}
+
+myOtherBox.addEventListener("click", function () {
+  // uses a fuse
+  grow();
+  console.log("grew");
+});
